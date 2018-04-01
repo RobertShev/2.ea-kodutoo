147892c49017c31558a1412a0325ef6bd853480b
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-04-01 16:18:30 +0300

    From Scrach
    Start project from scrach

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 /* TYPER */
 const TYPER = function () {
-	console.log(0);
   if (TYPER.instance_) {
     return TYPER.instance_
   }
@@ -15,15 +14,13 @@ const TYPER = function () {
   this.word = null
   this.wordMinLength = 5
   this.guessedWords = 0
-  this.score = 0
-  this.combo = 0
+
   this.init()
 }
 
 window.TYPER = TYPER
 
 TYPER.prototype = {
-
   init: function () {
     this.canvas = document.getElementsByTagName('canvas')[0]
     this.ctx = this.canvas.getContext('2d')
@@ -34,14 +31,11 @@ TYPER.prototype = {
     this.canvas.width = this.WIDTH * 2
     this.canvas.height = this.HEIGHT * 2
 
-     this.loadWords()
-	console.log(1);
-	  
+    this.loadWords()
   },
 
   loadWords: function () {
     const xmlhttp = new XMLHttpRequest()
-	 console.log('8')
 
     xmlhttp.onreadystatechange = function () {
       if (xmlhttp.readyState === 4 && (xmlhttp.status === 200 || xmlhttp.status === 0)) {
@@ -49,6 +43,7 @@ TYPER.prototype = {
         const wordsFromFile = response.split('\n')
 
         typer.words = structureArrayByWordLength(wordsFromFile)
+
         typer.start()
       }
     }
@@ -58,56 +53,38 @@ TYPER.prototype = {
   },
 
   start: function () {
-	  console.log('2')
     this.generateWord()
     this.word.Draw()
+
     window.addEventListener('keypress', this.keyPressed.bind(this))
-	
   },
 
   generateWord: function () {
-	  console.log('3')
     const generatedWordLength = this.wordMinLength + parseInt(this.guessedWords / 5)
     const randomIndex = (Math.random() * (this.words[generatedWordLength].length - 1)).toFixed()
     const wordFromArray = this.words[generatedWordLength][randomIndex]
 
-    this.word = new Word(wordFromArray, this.canvas, this.ctx)///mingi nendest tahendab sona 
+    this.word = new Word(wordFromArray, this.canvas, this.ctx)
   },
 
   keyPressed: function (event) {
-	console.log('7')
-	const letter = String.fromCharCode(event.which)
+    const letter = String.fromCharCode(event.which)
 
     if (letter === this.word.left.charAt(0)) {
-		this.word.removeFirstLetter()
-		this.combo += 1
-		this.guessedLetters += 1
-		if(this.combo > 8){
-			this.combo = 0
-		}
-	
-		this.score +=(this.guessedLetters+this.guessedWords)
-		//updatescore
-		if (this.word.left.length === 0) {
-			this.guessedWords += 1
-			this.score += (this.guessedLetters+this.guessedWords)*2
-			//updatescore
-				if(this.guessedWords == 30){
-					this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-					gameFinish();
-				}
+      this.word.removeFirstLetter()
 
-			this.generateWord()
-		}
+      if (this.word.left.length === 0) {
+        this.guessedWords += 1
 
-    this.word.Draw()
+        this.generateWord()
+      }
+
+      this.word.Draw()
     }
-	
   }
-
+}
 
 /* WORD */
-//siin error : Uncaught SyntaxError: Unexpected token const line 111
 const Word = function (word, canvas, ctx) {
   this.word = word
   this.left = this.word
@@ -117,7 +94,6 @@ const Word = function (word, canvas, ctx) {
 
 Word.prototype = {
   Draw: function () {
-	  console.log('4')
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
     this.ctx.textAlign = 'center'
@@ -133,32 +109,18 @@ Word.prototype = {
 /* HELPERS */
 function structureArrayByWordLength (words) {
   let tempArray = []
-   console.log('5')
 
   for (let i = 0; i < words.length; i++) {
     const wordLength = words[i].length
     if (tempArray[wordLength] === undefined)tempArray[wordLength] = []
+
     tempArray[wordLength].push(words[i])
   }
 
   return tempArray
-  
 }
-
-+function checkNameInput() {
-  let x = document.getElementById("nameText").value;
-  if (document.getElementById("nameText").value != ""){
-    startGame()
-  } else {
-    alert("Name field is empty!")
-  }
-}
-//puudu veel update score, gameFinisgja gameStart
-
 
 window.onload = function () {
-	 console.log('6')
   const typer = new TYPER()
   window.typer = typer
-
 }
