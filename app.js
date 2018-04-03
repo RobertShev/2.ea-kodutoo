6999d2e93e8c848ca1ff2e2a4e48e249104c66ff
Robert Shevchenko <25097046+RobertShev@users.noreply.github.com>
2018-04-04 00:51:27 +0300

    App works
    added game ended alert
    added result counting
    added localstorage
    -----------
    missisng toplist show
    missing daynight mode
    missing change fonts on enter key

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
 let solvedWords = 0;
+let helpDate = new Date();
+let helpTime = helpDate.getTime()/1000;
 
+function reviewLocalStorage(){
+  for (let l = 1; l<11; l++) {
+    if ((localStorage.getItem('result' + l)) == 'null' || (localStorage.getItem('result' + l)) == 0) {
+      localStorage.setItem('result' + l,0);
+      localStorage.setItem('resultShow' + l, "unknown");
+    }
+  }
+}
 
 /*Score Counting*/
 function result (){
-  let date = new Date();
-  let time =date.getTime()/1000;
-  document.getElementById('timeLeft').innerHTML = "Time left : "+ time;
-  document.getElementById('result').innerHTML = "Current score : "+solvedWords;
+  let update = setInterval(function(){
+    let date = new Date();
+    let time = date.getTime()/1000;
+    let timeLeft = Math.round(time-helpTime);
+    let gameResult = ((newWord.length*solvedWords)-timeLeft);
+    document.getElementById('timeLeft').innerHTML = "Time left : " + timeLeft +"/30";
+    document.getElementById('result').innerHTML = "Current score : " + gameResult;
+    document.addEventListener("keydown", function(event) {
+      //if enter pressed  
+      if(event.keyCode = 13){
+        //add here changing fonts if enter pressed
+      }
+    })
+    if (timeLeft>29){
+      for (let i = 1; i< 11; i++){
+        if (Number(gameResult) > localStorage.getItem('result'+1)){
+          for (let j = 1; j < (11-j); j++){
+            localStorage.setItem('result'+(11-j), localStorage.getItem('result'+(10-j)));
+            localStorage.setItem('resultShow'+(11-j), localStorage.getItem('resultShow' + (10-j)));
+          }
+          localStorage.setItem('result'+i,Number(gameResult));
+          localStorage.setItem('resultShow'+i, document.getElementById('gamerName').value);
+          solvedWords = 0;
+          break;
+        }
+      }
+      clearInterval(update);
+      alert("Game is over "+ localStorage.getItem('resultShow1') +", your result is " + gameResult + " points");
+    }
+  })
 }
 
 /*Show Results*/
 //
+function showResults(){
+  //add results to result page from localstorage
+  reviewLocalStorage();
+  document.getElementById('record0').innerHTML = localStorage.getItem('result1') + " did " + localStorage.getItem('resultShow1');
+
+}
 
 /*Start Game*/
 function startGame(){
   typer.start();
+  let solvedWords = 0;
+  let helpDate = new Date();
+  let helpTime = helpDate.getTime()/1000;
   result();
 }
 
@@ -96,7 +141,7 @@ TYPER.prototype = {
         this.guessedWords += 1
         solvedWords ++;
 
-        this.generateWord()
+        this.generateWord();
       }
 
       this.word.Draw()
@@ -110,6 +155,7 @@ const Word = function (word, canvas, ctx) {
   this.left = this.word
   this.canvas = canvas
   this.ctx = ctx
+  newWord = this.left;
 }
 
 Word.prototype = {
@@ -119,6 +165,7 @@ Word.prototype = {
     this.ctx.textAlign = 'center'
     this.ctx.font = '140px Courier'
     this.ctx.fillText(this.left, this.canvas.width / 2, this.canvas.height / 2)
+    
   },
 
   removeFirstLetter: function () {
